d0d185401a997cf29ec0c2d557997a7a6a5189f1
RafaelFerreiraNunes <rafaelguarulhos@hotmail.com>
2026-03-07 23:28:25 -0300

    feat: repo inicial

---
+++ src/main/kotlin/com/raspberry/house_raspberry_out/HouseRaspberryOutApplication.kt
@@ -0,0 +1,11 @@
+package com.raspberry.house_raspberry_out
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HouseRaspberryOutApplication
+
+fun main(args: Array<String>) {
+	runApplication<HouseRaspberryOutApplication>(*args)
+}
