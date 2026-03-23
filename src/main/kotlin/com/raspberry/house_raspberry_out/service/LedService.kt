471aa2dbce49c8defe43923d38bbb66f40ef5ee2
RafaelFerreiraNunes <rafaelguarulhos@hotmail.com>
2026-03-23 08:46:11 -0300

    feat: ajuste logs

--- src/main/kotlin/com/raspberry/house_raspberry_out/service/LedService.kt
+++ src/main/kotlin/com/raspberry/house_raspberry_out/service/LedService.kt
@@ -2,10 +2,13 @@ package com.raspberry.house_raspberry_out.service
 
 import com.diozero.devices.LED
 import com.raspberry.house_raspberry_out.factory.LedFactory
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class LedService {
+
+    private val logger = LoggerFactory.getLogger(LedService::class.java)
 
     private val leds: MutableList<LED> = mutableListOf()
     val pins = intArrayOf(18,23,24,25,8,7)
@@ -18,6 +21,7 @@ class LedService {
 
     fun changeStatusLed(led: Int) {
         leds[led -1].toggle()
+        logger.info("Led $led changed")
     }
 
 }
